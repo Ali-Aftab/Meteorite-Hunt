64e71b6293172951fc0cf689c7f957acf3c6e96c
Fig Yogurt <itisaftab@gmail.com>
2018-10-20 16:37:48 -0400

    thunk is working

--- src/client/store/index.js
+++ src/client/store/index.js
@@ -3,16 +3,20 @@ import createLogger from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 
 export const GET_METEORITES = "GET_METEORITES";
+export const TOP_FIVE = "TOP_FIVE";
 
 let initialState = {
-  allMeteorites: []
+  allMeteorites: [],
+  topFive: []
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_METEORITES:
       return { ...state, allMeteorites: action.payload };
-
+    case TOP_FIVE: {
+      return { ...state, topFive: action.payload };
+    }
     default:
       return state;
   }
--- src/client/store/thunks.js
+++ src/client/store/thunks.js
@@ -1,9 +1,14 @@
 import axios from "axios";
-import { GET_METEORITES } from "./index";
+import geolib from "geolib";
+import { GET_METEORITES, TOP_FIVE } from "./index";
 
 const allMeteorites = payload => ({
   type: GET_METEORITES,
-  payload: payload
+  payload
+});
+const topFive = payload => ({
+  type: TOP_FIVE,
+  payload
 });
 
 export const gettingAllMeteorites = async dispatch => {
@@ -12,6 +17,12 @@ export const gettingAllMeteorites = async dispatch => {
       "https://data.nasa.gov/resource/gh4g-9sfh.json"
     );
     dispatch(allMeteorites(meteoriteInfo.data));
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const fetchTopFive = (long, lat, allMeteors) => dispatch => {
+  try {
   } catch (error) {
     console.log(error);
   }
