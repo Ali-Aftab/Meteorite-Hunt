7e9defdd3b53be62733b2e65cbcef91b1b762891
Fig Yogurt <itisaftab@gmail.com>
2018-10-20 15:50:17 -0400

    location implemented

--- src/client/store/thunks.js
+++ src/client/store/thunks.js
@@ -1,39 +1,18 @@
 import axios from "axios";
-const API_KEY = process.env.REACT_APP_API_KEY;
+import { GET_METEORITES } from "./index";
 
-// const getFiveCities = payload => ({
-//   type: GET_FIVE_CITIES,
-//   payload
-// });
+const allMeteorites = payload => ({
+  type: GET_METEORITES,
+  payload: payload
+});
 
-// const getAnyCity = payload => ({
-//   type: GET_ANY_CITY,
-//   city: payload.city,
-//   cityForecast: payload.cityForecast
-// });
-
-export const gettingAnyCity = id => async dispatch => {
+export const gettingAllMeteorites = async dispatch => {
   try {
-    const city = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?id=${id}&units=imperial&APPID=${API_KEY}`
+    const meteoriteInfo = await axios.get(
+      "https://data.nasa.gov/resource/gh4g-9sfh.json"
     );
-    const forecast = await axios.get(
-      `https://api.openweathermap.org/data/2.5/forecast?id=${id}&units=imperial&APPID=${API_KEY}`
-    );
-    dispatch(getAnyCity({ city: city.data, cityForecast: forecast.data.list }));
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-export const gettingFiveCities = async dispatch => {
-  try {
-    const { data } = await axios.get(
-      `https://api.openweathermap.org/data/2.5/group?id=5128581,4684888,5368361,4930956,4372599&units=imperial&APPID=${API_KEY}`
-    );
-
-    dispatch(getFiveCities(data));
-  } catch (err) {
-    console.log(err);
+    dispatch(allMeteorites(meteoriteInfo.data));
+  } catch (error) {
+    console.log(error);
   }
 };
